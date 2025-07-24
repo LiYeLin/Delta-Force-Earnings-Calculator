9c1ee7e7015be89ccb2a0a39c7dce8a311d362dc
EnHe <im.liyelin@gmail.com>
2025-07-24 11:13:55 +0000

    创建项目

---
+++ lcsjz/src/main/java/com/sjz/lcsjz/LcsjzApplication.java
@@ -0,0 +1,14 @@
+package com.sjz.lcsjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcsjzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcsjzApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ lcsjz/src/test/java/com/sjz/lcsjz/LcsjzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjz.lcsjz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcsjzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
