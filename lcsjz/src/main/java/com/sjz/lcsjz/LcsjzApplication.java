828387a8aa91bee42b390373688a7dae05d60d59
EnHe <im.liyelin@gmail.com>
2025-07-28 07:15:20 +0000

    目前有点问题 待解决

--- lcsjz/src/main/java/com/sjz/lcsjz/LcsjzApplication.java
+++ lcsjz/src/main/java/com/sjz/lcsjz/LcsjzApplication.java
@@ -1,19 +1,18 @@
 package com.sjz.lcsjz;
 
 
-import org.apache.logging.slf4j.SLF4JLogger;
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sjz.lcsjz.common.dal.mapper")
 public class LcsjzApplication {
     private static final Logger log = LoggerFactory.getLogger(LcsjzApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(LcsjzApplication.class, args);
 		log.info("启动成功");
 	}
-
 }
---
+++ lcsjz/src/main/java/com/sjz/lcsjz/common/dal/mapper/ItemMapper.java
@@ -0,0 +1,9 @@
+package com.sjz.lcsjz.common.dal.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sjz.lcsjz.common.dal.dao.Item;
+
+public interface ItemMapper extends BaseMapper<Item>{
+
+}
