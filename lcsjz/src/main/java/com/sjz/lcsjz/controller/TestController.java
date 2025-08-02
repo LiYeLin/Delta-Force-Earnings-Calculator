c96bf36e5e586dc7d637afcf74ad55c70a17dbeb
liyelin <im.liyelin@gmail.com>
2025-08-02 15:51:58 +0800

    fix：去掉snapshot

--- lcsjz/src/main/java/com/sjz/lcsjz/controller/TestController.java
+++ lcsjz/src/main/java/com/sjz/lcsjz/controller/TestController.java
@@ -3,6 +3,8 @@ package com.sjz.lcsjz.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 测试
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author enhe
  * @since 2025-07-29
  */
-@Controller
+@RestController
 @RequestMapping("/sjz-lc")
 public class TestController {
+
     @GetMapping
+    @ResponseBody
     public String verify() {
         return "success";
     }
